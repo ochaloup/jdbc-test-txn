ff70a02d25857031e3f2dc6731cf84763cb3f19c
Ondra Chaloupka <ochaloup@redhat.com>
2017-12-05 01:20:42 +0100

    something

---
+++ src/main/java/io/narayana/test/DBUtils.java
@@ -0,0 +1,37 @@
+package io.narayana.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+	private static final String DB_NAME = "crashrec";
+    private static final String DB_DRIVER = "org.postgresql.Driver";
+    private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/" + DB_NAME;
+    private static final String DB_USER = "crashrec";
+    private static final String DB_PASSWORD = "crashrec";
+
+    public static String TABLE1_NAME = "TABLE1";
+    public static String TABLE2_NAME = "TABLE2";
+
+    public static String INSERT_STATEMENT_T1 = String.format("INSERT INTO %s (id, node_name, random) values (?, ?, ?)", TABLE1_NAME);
+    public static String INSERT_STATEMENT_T2 = String.format("INSERT INTO %s (id, value) values (?, ?)", TABLE2_NAME);
+    public static String SELECT_ALL = "SELECT * FROM %s" ;
+    public static String SELECT_WHERE = "SELECT * FROM %s WHERE node_name = ? AND random = ?" ;
+
+    public static Connection getDBConnection(String dbname) {
+        Connection dbConnection = null;
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            String dbConnectionUrl = String.format(DB_CONNECTION, dbname);
+            dbConnection = DriverManager.getConnection(dbConnectionUrl, DB_USER, DB_PASSWORD);
+            return dbConnection;
+        } catch (SQLException e) {
+            throw new IllegalStateException("Can't get connection to inmem H2 database " + dbname, e);
+        }
+    }
+}

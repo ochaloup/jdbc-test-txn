f32b7663e64a2a670e74cc44e917c1c3aabcbdeb
Ondra Chaloupka <ochaloup@redhat.com>
2017-12-08 09:23:31 +0100

    tuning startup scripts

--- postgresql/psql-runner.sh
+++ postgresql/psql-runner.sh
@@ -1,9 +1,23 @@
+#!/bin/bash
+
+# starting postgres now
+set -x
+run-postgresql "$@" &
+
+# can't redirect error output to somewhere in docker probaly... 2&>1
+function test {
+  psql -l | grep -q $POSTGRESQL_DATABASE
+}
+while ! test; do sleep 1; done
 
 # iterating over all sql scripts
-find . -type f -name '/docker-entrypoint-initdb.d/*.sql' -exec sh -c '
+find /docker-entrypoint-initdb.d -type f -name '*.sql' -exec sh -c '
   file="$0"
   echo "$file"
+  PGPASSWORD=$POSTGRESQL_PASSWORD psql -h localhost $POSTGRESQL_DATABASE $POSTGRESQL_USER -a -f "$file"
 ' {} ';'
-# PGPASSWORD=$POSTGRESQL_PASSWORD psql -h localhost $POSTGRESQL_DATABASE $POSTGRESQL_USER -a -f ./psql_init.sql
+
+kill %1
+while [ -e '/var/lib/pgsql/data/userdata/postmaster.pid' ]; do sleep 1; done
 
 exec run-postgresql "$@"
